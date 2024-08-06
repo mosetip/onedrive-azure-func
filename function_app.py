e75a24efea2f9da3dac9ac768011e250be45a654
mosetip <derrick.moseti@pawait.co.ke>
2024-08-06 14:17:25 +0300

    modified auth leve access for azure function

--- function_app.py
+++ function_app.py
@@ -3,7 +3,9 @@ import os
 import subprocess
 import requests
 import tempfile
+
 import azure.functions as func
+
 
 app = func.FunctionApp()  # Create an instance of FunctionApp
 
@@ -44,3 +46,25 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
         return func.HttpResponse(f"An error occurred: {str(e)}", status_code=500)
+
+
+@app.route(route="http_trigger", auth_level=func.AuthLevel.FUNCTION)
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
